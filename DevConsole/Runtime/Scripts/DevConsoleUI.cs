26d390e33fb117a90e57b9924751efe5ba0e9b00
Dan <igaryhe@gmail.com>
2021-04-14 22:06:40 +0800

    up & down arrow to navigate through history

--- DevConsole/Runtime/Scripts/DevConsoleUI.cs
+++ DevConsole/Runtime/Scripts/DevConsoleUI.cs
@@ -11,6 +11,7 @@ namespace DevConsole
         private TextMeshProUGUI _output;
         private TMP_InputField _input;
         private List<string> _history;
+        private int _backlogIdx;
 
         public DevConsoleUI()
         {
@@ -27,6 +28,17 @@ namespace DevConsole
         private void Update()
         {
             if (Keyboard.current.enterKey.wasPressedThisFrame) ExecuteCommand(_input.text);
+            else if (Keyboard.current.upArrowKey.wasPressedThisFrame)
+            {
+                if (_backlogIdx < 0) return;
+                _input.text = _history[_backlogIdx];
+                if (_backlogIdx > 0) _backlogIdx--;
+            } else if (Keyboard.current.downArrowKey.wasPressedThisFrame)
+            {
+                if (_backlogIdx > _history.Count - 1) return;
+                _input.text = _history[_backlogIdx];
+                if (_backlogIdx < _history.Count - 1) _backlogIdx++;
+            }
         }
 
         public void Log(string message) => _output.text += $"{message}\n";
@@ -37,6 +49,7 @@ namespace DevConsole
         {
             var (command, args) = ParseCommand(input);
             _history.Add(input);
+            _backlogIdx = _history.Count - 1;
             Log($"> {input}");
             DevConsole.ExecuteCommand(command, args);
             _input.text = Empty;
